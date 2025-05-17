97e57835d158c0b3f8775c30dfaa897f9903e535
Mateusz Damian Chmurzyski <m.chmurzynski@stud.elka.pw.edu.pl>
2025-05-17 13:38:44 +0200

    P7 | testy

--- main.py
+++ main.py
@@ -5,11 +5,16 @@ from sklearn.metrics import accuracy_score, confusion_matrix, classification_rep
 from matplotlib import pyplot as plt
 import seaborn as sns
 
+
 def load_split_data():
     iris = fetch_ucirepo(id=53)
 
     X = iris.data.features.to_numpy()
     y = iris.data.targets.to_numpy().ravel()
+
+    X_min = X.min(axis=0)
+    X_max = X.max(axis=0)
+    X = (X - X_min) / (X_max - X_min)
 
     test_fraction = 0.2
     test_number = int(test_fraction * X.shape[0])
@@ -22,13 +27,14 @@ def load_split_data():
     y_test = y[test_indices]
     return X_train, y_train, X_test, y_test
 
-def test_NB():
+
+def test_NB(distribution):
     X_train, y_train, X_test, y_test = load_split_data()
 
     nb = NaiveBayes()
     nb.fit(X_train, y_train)
 
-    y_pred = nb.predict(X_test)
+    y_pred = nb.predict(X_test, distribution)
     print(y_pred)
     print(y_test)
     print("Dokładność:", accuracy_score(y_test, y_pred))
@@ -42,13 +48,20 @@ def test_NB():
     print(cm)
 
     plt.figure(figsize=(6, 4))
-    sns.heatmap(np.array(cm), annot=True, fmt='d', cmap='Blues',
-                xticklabels=labels, yticklabels=labels)
-    plt.xlabel('Przewidziana klasa')
-    plt.ylabel('Prawdziwa klasa')
-    plt.title('Macierz pomyłek')
+    sns.heatmap(
+        np.array(cm),
+        annot=True,
+        fmt="d",
+        cmap="Blues",
+        xticklabels=labels,
+        yticklabels=labels,
+    )
+    plt.xlabel("Przewidziana klasa")
+    plt.ylabel("Prawdziwa klasa")
+    plt.title("Macierz pomyłek")
     plt.show()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     np.random.seed(42)
-    test_NB()
+    test_NB("beta")
